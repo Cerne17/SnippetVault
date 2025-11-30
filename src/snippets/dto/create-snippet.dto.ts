27bdecd9fe5fa4841f085fe3e3722c0d1bd4d070
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-11-29 22:32:17 -0300

    feat: creates the snippet creation dto

--- src/snippets/dto/create-snippet.dto.ts
+++ src/snippets/dto/create-snippet.dto.ts
@@ -1,1 +1,24 @@
-export class CreateSnippetDto {}
+import { IsString, IsNotEmpty, IsArray, IsOptional } from "class-validator";
+
+export class CreateSnippetDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  code: string;
+
+  @IsString()
+  @IsNotEmpty()
+  language: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  tags?: string[];
+
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
+}
